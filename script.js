ff0fd2022fc07337cfed157f78482d4151fdf77a
Web-Developer-DANIS <158835501+Web-Developer-DANIS@users.noreply.github.com>
2024-05-14 09:27:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const userName = prompt('введите имя');
+const userAge = +prompt('введите своё имя');
+console.log("Меня завут:",userName);
+console.log("Мой возраст:",userAge);
+
+const firstNumber = +prompt('Решите пример: 6 + 3 = ?')
+const secondNumber = +prompt('Решите пример: 20 - 5 = ?')
+const sertNumber = +prompt('Решите пример: 15 * 2 = ?')
+const forsNumber = +prompt('Решите пример: 8 / 4 = ?')
+const feefstNumber = +prompt('Решите пример: 10 % 3 = ?')
+
+if (firstNumber == 9) {
+    console.log('ваш ответ правильный');
+} else {
+    console.log('ваш ответ неправильный');
+}
+
+if (secondNumber == 15) {
+    console.log('ваш ответ правильный');
+} else {
+    console.log('ваш ответ неправильный');
+}
+
+if (sertNumber == 30) {
+    console.log('ваш ответ правильный');
+} else {
+    console.log('ваш ответ неправильный');
+}
+
+if (forsNumber == 2) {
+    console.log('ваш ответ правильный');
+} else {
+    console.log('ваш ответ неправильный');
+}
+
+if (feefstNumber == 0.3) {
+    console.log('ваш ответ правильный');
+} else {
+    console.log('ваш ответ неправильный');
+}
+
